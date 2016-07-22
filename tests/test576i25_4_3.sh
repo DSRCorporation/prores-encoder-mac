85d1090bf66f6cd7cb57235bde8cd18a795eb6ad
Grigoriy Gavrilov <grigoriy.gavrilov@dsr-company.com>
2016-07-22 14:44:48 +0300

    Fix test scripts formats

--- tests/test576i25_4_3.sh
+++ tests/test576i25_4_3.sh
@@ -2,6 +2,6 @@
 
 [ -z "$1" ] && echo "Please set an input file." && exit -1
 
-FORMAT=scale=720:576,fps=25000/1001
+FORMAT=scale=720:576,fps=25/1
 ffmpeg -i $1 -an -t 10 -f rawvideo -pix_fmt yuv422p16le -vf $FORMAT - | \
     ../build/prenc -f $FORMAT,setdar=4/3,interlace `basename $0 .sh`.mov
